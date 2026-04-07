93b81ffb814e405df48d2b3f5408c6e7ff720211
Collins Naidu <collinsnaidu@gmail.com>
2026-04-07 22:08:45 +0530

    chore: scaffold Angular 15.2 app with Angular Material and responsive setup

---
+++ src/app/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule
+  ]
+})
+export class MaterialModule {}
